67f3c14554b6ea5172861ae0a0284fba6d8ccbcf
yuki <yymmt3.waste@gmail.com>
2019-01-26 20:34:46 +0900

    add xcode project

---
+++ CastSDK-ios-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CastSDK-ios-demo
+//
+//  Created by Yuki Yamamoto on 2019/01/26.
+//  Copyright © 2019 Yuki Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
