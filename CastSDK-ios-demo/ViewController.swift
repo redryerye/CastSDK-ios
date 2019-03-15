64ec19646dc36f03de90c4ae410d5edb5d882ed6
yuki <yymmt3.waste@gmail.com>
2019-03-15 19:29:49 +0900

    add image request

--- CastSDK-ios-demo/ViewController.swift
+++ CastSDK-ios-demo/ViewController.swift
@@ -24,8 +24,6 @@ class ViewController: UIViewController, GCKSessionManagerListener, GCKRemoteMedi
         
         sessionManager = GCKCastContext.sharedInstance().sessionManager
         sessionManager.add(self)
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,8 +42,50 @@ class ViewController: UIViewController, GCKSessionManagerListener, GCKRemoteMedi
         castButton.center = CGPoint(x: view.center.x, y: view.center.y + 100)
     }
     
+    func loadImage() {
+        if sessionManager.currentSession != nil {
+            sendImage()
+        } else {
+            print("current session is \(sessionManager.currentSession)")
+        }
+    }
+    
+    func sendImage() {
+        let metadata = GCKMediaMetadata()
+        metadata.setString("Big Buck Bunny (2008)", forKey: kGCKMetadataKeyTitle)
+        metadata.setString("Big Buck Bunny tells the story of a giant rabbit with a heart bigger than " +
+            "himself. When one sunny day three rodents rudely harass him, something " +
+            "snaps... and the rabbit ain't no bunny anymore! In the typical cartoon " +
+            "tradition he prepares the nasty rodents a comical revenge.",
+                           forKey: kGCKMetadataKeySubtitle)
+        metadata.addImage(GCKImage(url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg")!,
+                                   width: 480,
+                                   height: 360))
+        
+        // Define information about the media item.
+        let url = URL(string: "https://www.gstatic.com/webp/gallery3/1.png")
+        guard let mediaURL = url else {
+            print("invalid mediaURL")
+            return
+        }
+        
+        let mediaInfoBuilder = GCKMediaInformationBuilder(contentURL: mediaURL)
+        mediaInfoBuilder.contentID = "https://www.gstatic.com/webp/gallery3/1.png"
+        mediaInfoBuilder.streamType = GCKMediaStreamType.none
+        mediaInfoBuilder.contentType = "image/png"
+        mediaInfoBuilder.metadata = metadata
+        mediaInformation = mediaInfoBuilder.build()
+        
+        if let request = sessionManager.currentSession?.remoteMediaClient?.loadMedia(mediaInformation!) {
+            request.delegate = self
+        }
+        
+    }
+    
     @IBAction func askForCurrentSession(_ sender: Any) {
         print("current session: \(String(describing: sessionManager.currentSession))")
+        
+        loadImage()
     }
     
     // MARK: - GCKSessionManagerListener
